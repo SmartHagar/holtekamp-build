556f96a2262a43c2afdfbdd2c894c07637ee8cef
SmartHagar <smarthagar@gmail.com>
2023-09-19 21:20:24 +0900

    menambahkan halaman index pada admin

---
+++ _next/static/chunks/app/(user)/gambarKegiatan/layout-4b8aaa80c70d6b1a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[181,812,651,965,344,767,270,323,505,744,883],{3521:function(){}},function(n){n.O(0,[971,596,560],function(){return n(n.s=3521)}),_N_E=n.O()}]);
